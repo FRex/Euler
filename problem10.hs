a190da84806acce600b8414549e9f5d9403f471e
FRex <rex2k8@gmail.com>
2018-04-06 16:10:47 +0200

    Problem 10 in Haskell.

---
+++ problem10.hs
@@ -0,0 +1,8 @@
+-- proper sieve from The Genuine Sieve of Eratosthenes by Melissa E. O'Neill
+primes = 2 : [x | x <- [3..], isprime x]
+    where
+        isprime x = all (\p -> x `mod` p > 0) $ totry x
+        totry x = takeWhile (\p -> p * p <= x) primes
+
+main = do
+    print $ sum $ takeWhile (< 2000000) primes
